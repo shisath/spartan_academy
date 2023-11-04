5211dd238b11f6ba6e97a4552ce7b9801bf4dd1b
Immanuel <imman.immanuel100@gmail.com>
2023-11-04 22:07:00 +0530

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Hi there.'),
+        child: Text('Hi Sathish !'),
       ),
     );
   }
